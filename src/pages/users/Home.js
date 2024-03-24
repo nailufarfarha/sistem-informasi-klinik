18cee217a8e8e83462dbd057a6be3ab11519102d
nailufarfarha <nailufarfaah@gmail.com>
2024-03-25 00:27:57 +0700

    add layanan, fasilitas & footer

--- src/pages/users/Home.js
+++ src/pages/users/Home.js
@@ -5,9 +5,6 @@ import clinic from "../../assets/clinic.jpeg";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import logo from "../../assets/logo.png";
-import Card from "react-bootstrap/Card";
-import CardGroup from "react-bootstrap/CardGroup";
-import icon_pomum from "../../assets/poli-umum.png";
 
 const Home = () => {
   return (
@@ -41,29 +38,69 @@ const Home = () => {
           <span>Layanan </span>
           <span className="text-orange">FATIMAH MEDICAL CLINIC</span>
         </h2>
-        <CardGroup className="card-layanan">
-          <Card style={{ backgroundColor: "transparent", border: "none", boxShadow: "none" }}>
-            <Card.Img variant="top" src={icon_pomum} />
-            <Card.Body>
-              <Card.Title>Poli Umum</Card.Title>
-            </Card.Body>
-          </Card>
-          <Card style={{ backgroundColor: "transparent", border: "none", boxShadow: "none" }}>
-            <Card.Img variant="top" src="holder.js/100px160" />
-            <Card.Body>
-              <Card.Title>Card title</Card.Title>
-            </Card.Body>
-          </Card>
-          <Card style={{ backgroundColor: "transparent", border: "none", boxShadow: "none" }}>
-            <Card.Img variant="top" src="holder.js/100px160" />
-            <Card.Body>
-              <Card.Title>Card title</Card.Title>
-            </Card.Body>
-          </Card>
-        </CardGroup>
+        <div className="grid-container">
+          <div className="card">
+            <img src={require("../../assets/poli-umum.png")} alt="Poli Umum" style={{ width: "60px", height: "60px" }} />
+            <h6>Poli Umum</h6>
+          </div>
+          <div className="card">
+            <img src={require("../../assets/polikia.png")} alt="Poli Kia" />
+            <h6>Poli Kia</h6>
+          </div>
+          <div className="card">
+            <img src={require("../../assets/igd.png")} alt="IGD 24 Jam" style={{ width: "60px", height: "60px" }} />
+            <h6>IGD 24 Jam</h6>
+          </div>
+          <div className="card">
+            <img src={require("../../assets/farmasi.png")} alt="Farmasi" style={{ width: "55px", height: "55px" }} />
+            <h6>Farmasi</h6>
+          </div>
+          <div className="card">
+            <img src={require("../../assets/laboratory.png")} alt="Laboratorium" style={{ width: "55px", height: "55px" }} />
+            <h6>Laboratorium</h6>
+          </div>
+          <div className="card">
+            <img src={require("../../assets/homecare.png")} alt="Home Care" style={{ width: "60px", height: "60px" }} />
+            <h6>Home Care</h6>
+          </div>
+          <div className="card">
+            <img src={require("../../assets/medis.png")} alt="Tindakan Medis" style={{ width: "60px", height: "60px" }} />
+            <h6>Tindakan Medis</h6>
+          </div>
+        </div>
       </div>
-      <div className="fasilitas"></div>
-      <div className="footer"></div>
+      <div className="fasilitas">
+        <h2 className="fasilitas-header">
+          <span>Fasilitas </span>
+          <span className="text-orange">FATIMAH MEDICAL CLINIC</span>
+        </h2>
+        <div className="grid-container-fas">
+          <div className="card">
+            <img src={clinic} alt="IGD" />
+          </div>
+          <div className="card">
+            <img src={clinic} alt="IGD" />
+          </div>
+          <div className="card">
+            <img src={clinic} alt="IGD" />
+          </div>
+        </div>
+      </div>
+      <div className="footer">
+        <div className="footer-column">
+          <img src={require("../../assets/logofooter.png")} />
+        </div>
+        <div className="footer-column">
+          <h5>Hubungi Kami</h5>
+          <p>08123456789</p>
+          <p>fatimahmedicalclinic@gmail.com</p>
+          <p>Jl. Cikeris, Desa Cikeris, Kecamatan Bojong, Kabupaten Purwakarta</p>
+        </div>
+        <div className="footer-column">
+          <h5>Sosial Media</h5>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi auctor lorem et ipsum luctus, ut feugiat velit fermentum.</p>
+        </div>
+      </div>
     </div>
   );
 };
